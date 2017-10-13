b7421b7f29e50594a4546929fd2da0c9c3e75d57
Armaan Amirani <armaanamirani@gmail.com>
2017-10-12 21:49:57 -0500

    Created viewUsers.php w MYSQL connection

---
+++ Excercise2/viewUsers.php
@@ -0,0 +1,12 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "aamirani", 'P@$$word123', "aamirani");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+/* close connection */
+$mysqli->close();
+?>
