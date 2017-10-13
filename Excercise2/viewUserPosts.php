79379af26a80460ae4bbca183f11fa62046e2892
Armaan Amirani <armaanamirani@gmail.com>
2017-10-12 22:36:10 -0500

    Added submit button

--- Excercise2/viewUserPosts.php
+++ Excercise2/viewUserPosts.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>View User POsts</title>
+    <title>View User Posts</title>
 </head>
 <body>
     <form action="viewUserPosts.php">
@@ -29,6 +29,7 @@
         $mysqli->close();
         ?>
         </select>
+        <button type="submit">Submit</button>
     </form>
 </body>
 </html>
